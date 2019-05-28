eea8afd280e223b3ee222a83ff4efef2252c3f98
Logan Willett <lwillett@pgsioffice.com>
2019-05-28 09:58:53 -0400

    Sadness

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+    private heroesUrl = 'api/heroes'; //URL to web
 
   getHeroes(): Observable<Hero[]> {
     // sends message after fetching the heroes\
@@ -15,7 +18,16 @@ export class HeroService {
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
-  
+  private log(message: string) {
+    this.messageService.add('HeroService: ${message}');
+  }
+
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
 }
---
+++ src/app/mock-hero.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr.Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 14, name: 'Bombasto' },
+    { id: 15, name: 'Celeritas' },
+    { id: 16, name: 'Magneta' },
+    { id: 13, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr. IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+    { id: 21, name: 'Grave Master' },
+    { id: 22, name: 'Bland Man' },
+];
